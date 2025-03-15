d8dc9f8cb2dca04618c0dc66a3a03ac71c62c176
tivereidoro <tivereidoro@yahoo.com>
2025-03-15 22:20:18 +0100

    Starting with the footer

---
+++ public-speaking-hub/src/components/shared/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <div>
+      <footer>
+        <div className="footer_container">
+          <div className="site_links"></div>
+        </div>
+      </footer>
+    </div>
+  );
+}
